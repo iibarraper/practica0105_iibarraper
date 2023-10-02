e8c738248a5ad0e774143aae2b3c7d6d977a1b83
iibarraper <iibarraper@educacion.navarra.es>
2023-10-02 17:44:41 +0200

    Ejercicio 6

---
+++ Actividad 6.py
@@ -0,0 +1,6 @@
+n = int(input("Di un entero positivo: "))
+if n < 1:
+        print("Por favor, di un entero positivo. ")
+else:
+      suma = (n * (n +1 )) // 2
+print("La suma desde 1 hasta", n, "es:", suma)
