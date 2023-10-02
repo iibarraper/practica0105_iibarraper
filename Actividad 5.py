baffea87b8b21a11c8822df23770274fd6e147fa
iibarraper <iibarraper@educacion.navarra.es>
2023-10-02 17:43:33 +0200

    Ejercicio 5

---
+++ Actividad 5.py
@@ -0,0 +1,4 @@
+horas_trabajadas = float(input("¿Cuantas horas has trabajado?"))
+costo_por_hora = float(input("¿Cuanto cobras por hora?"))
+paga = horas_trabajadas * costo_por_hora
+print("Tu paga es de: $"+str(paga))
