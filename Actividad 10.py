0cac69542ff7d4681ca8277b0aae660cc90bc03d
iibarraper <iibarraper@educacion.navarra.es>
2023-10-02 17:49:11 +0200

    Ejercicio 10

---
+++ Actividad 10.py
@@ -0,0 +1,6 @@
+peso_payaso = 112
+peso_muñeca = 75
+num_payasos = int(input("Payasos vendidos:"))
+num_muñecas = int(input("Muñecas vendidas:"))
+peso_total = (num_payasos * peso_payaso) + (num_muñecas * peso_muñeca)
+print(" Peso total del paquete", peso_total , "gramos")
