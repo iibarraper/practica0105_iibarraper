8fb1ecd635bfbd9b820472df14660a789578d017
iibarraper <iibarraper@educacion.navarra.es>
2023-10-02 17:46:18 +0200

    Ejercicio 8

---
+++ Actividad 8.py
@@ -0,0 +1,5 @@
+n = int(input(" Dime el primer número entero: "))
+m = int(input(" Dime el segundo número entero: "))
+c = n // m
+r = n % m
+print(f" {n} entre {m} da un cociente {c} y un resto {r} ")
